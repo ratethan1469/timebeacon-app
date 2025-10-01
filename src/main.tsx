b951ffa17ac0ac92bde040b460e39eaffd02e98e
ratethan1469 <ethan.ratnowsky@gmail.com>
2025-10-01 18:18:09 -0400

    Fix 502 error and trigger redeploy

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-// Force cache refresh - v1.0.2
+// Auth system v1.0.2 - Working!
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
